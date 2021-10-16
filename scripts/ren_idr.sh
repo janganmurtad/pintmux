a628969cc9785672a2c956f45cdeac58a2b6b084
Rifki Alfaridzi <alfaridzi48@gmail.com>
2021-10-16 12:00:44 +0700

    remove online check

--- scripts/ren_idr.sh
+++ scripts/ren_idr.sh
@@ -3,19 +3,10 @@
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
 source "$CURRENT_DIR/helpers.sh"
 
-api_status=$(curl -s  https://api.kraken.com/0/public/SystemStatus | jq '.result.status'| sed 's/\"//g')
-
 get_price()
 {
     price=$(curl -s https://api.pintukripto.com/v1/trade/exchange-rate/ren | jq '.payload.price.close' | sed 's/\"//g')
-
-    if [[ $api_status == 'online' ]]; then
-        echo "$price" | bc -l | awk '{printf "REN: %.i", $1}'
-    elif [[ $api_status == 'offline' ]]; then
-        echo "API offline"
-    else
-        echo "Error, No internet"
-    fi
+    echo "$price" | bc -l | awk '{printf "REN: %.i", $1}'
 }
 
 get_price
